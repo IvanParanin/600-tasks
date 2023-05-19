759fbe569d70410eadc20a1eb4919f00922d4f6f
Ivan <ivanparanin@bk.ru>
2023-05-19 18:07:37 +0600

    new task

--- src/java/com/ivanparanin/chapter12/Ex12_11.java
+++ src/java/com/ivanparanin/chapter12/Ex12_11.java
@@ -4,18 +4,18 @@ public class Ex12_11 {
     static Scanner reader = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int i, count1 = 0, count2 = 0, stud = 10;
-        for (i = 0; i < 10; i++) {
+        int i, count1 = 0, count2 = 0;
+        for (i = 0; i < 200; i++) {
             int answr = reader.nextInt();
-            if (answr == 1) {
+            if (answr == 1)
                 count1++;
-            } else if (answr == 0) { // todo
+            if (answr == 0)
                 count2++;
-            }
-            if (count1 > (i / 2))
-                System.out.println(count1 + " учеников проголосовали за и экскурсия состоится");
-            if (count2 < (i / 2))
-                System.out.println(count2 + " учеников проголосовали против и экскурсия не состоится");
         }
+        if (count1 > (i / 2))
+          System.out.println(count1 + " учеников проголосовали за и экскурсия состоится");
+        if (count2 > (i / 2))
+            System.out.println(count2 + " учеников проголосовали против и экскурсия не состоится");
     }
 }
+
