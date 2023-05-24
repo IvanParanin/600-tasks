e710d051687a371685bdc7d21a0fc8f171ea5cff
Ivan <ivanparanin@bk.ru>
2023-05-24 16:47:45 +0600

    new task

--- src/java/com/ivanparanin/chapter13/Ex13_3.java
+++ src/java/com/ivanparanin/chapter13/Ex13_3.java
@@ -1,11 +1,12 @@
 package com.ivanparanin.chapter13;
 
-public class Ex13_2 {
+public class Ex13_3 {
     public static void main(String[] args) {
         int gen, count = 0;
         for(int i = 0; i < 20; i++) {
-             gen = 10 + (int) (Math.random() * (99 - 10) + 1);
-             count += gen;
+            gen = 10 + (int) (Math.random() * (99 - 10) + 1);
+            if(gen % 2 == 0)
+               count += gen;
             System.out.print(gen + " ");
         }
         System.out.println("\nСумма " + count);
