0f9dac648772c7e60ab92a512ed2cc3b33728cbd
Ivan <ivanparanin@bk.ru>
2023-05-29 13:51:41 +0600

    new task

--- src/java/com/ivanparanin/chapter13/Ex13_15.java
+++ src/java/com/ivanparanin/chapter13/Ex13_15.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 public class Ex13_15 {
 static Scanner reader = new Scanner(System.in);
 public static void main(String[] args) {
-    int x = reader.nextInt(), a, b, sum = 0, sum1 = 0;
+    int x = reader.nextInt(), a, b, sum1 = 0, sum2 = 0;
     a = x / 10;
     b = x % 10;
     if(a > b) {
         for(int i = b; i <= a; i++) {
-            sum += i;
+            sum1 += i;
         }
     }
     if(a < b) {
         for(int i = a; i <= b; i++) {
-            sum1 += i;
+            sum2 += i;
         }
     }
-    System.out.println(sum);
     System.out.println(sum1);
+    System.out.println(sum2);
   }
 }
